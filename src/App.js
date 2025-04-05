ba49f681d7963426d39918451b9c97b7a0f1435b
mdeepanshu <deepanshumalviya8964@gmail.com>
2025-04-05 18:44:58 +0530

    deploy to prod

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useEffect, useState, useRef, use } from "react";
 
 import Header from "./features/header/header";
 import Home from "./features/home/home";
 import OurValues from "./features/our-values/our_values";
 import Footer from "./features/footer/footer";
 import FeaturedProjects from "./features/featured-projects/featured-projects";
+import AllProjects from "./features/all-projects/all-projects";
 
 function App() {
+  const [isMainPage, setMainPage] = useState(true);
+
+  const handlePage = (val) => {
+    setMainPage(val);
+  };
+
   return (
     <>
-      <Header></Header>
-      <Home></Home>
-      <OurValues></OurValues>
-      <FeaturedProjects></FeaturedProjects>
+      <Header handlePageChange={handlePage} isHomePage={isMainPage}></Header>
+      {isMainPage ? (
+        <>
+          <Home></Home>
+          <OurValues></OurValues>
+          <FeaturedProjects></FeaturedProjects>
+        </>
+      ) : (
+        <>
+          <AllProjects></AllProjects>
+        </>
+      )}
       <Footer></Footer>
     </>
   );
---
+++ src/features/all-projects/all-projects.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState, useRef, use } from "react";
+import left_line from "../../assets/svgs/left_line.svg";
+import right_line from "../../assets/svgs/right_line.svg";
+
+import property1 from "../../assets/allprojects/1.webp";
+import property2 from "../../assets/allprojects/2.webp";
+import property3 from "../../assets/allprojects/3.webp";
+import property4 from "../../assets/allprojects/4.webp";
+
+import styles from "./all-projects.module.css";
+
+function AllProjects() {
+  const [properties, setProperties] = useState([
+    {
+      property_img: property1,
+      property_name: "",
+      property_location: "",
+      price: "",
+      property_desc: "",
+    },
+    {
+      property_img: property2,
+      property_name: "",
+      property_location: "",
+      price: "",
+      property_desc: "",
+    },
+    {
+      property_img: property1,
+      property_name: "",
+      property_location: "",
+      price: "",
+      property_desc: "",
+    },
+    {
+      property_img: property2,
+      property_name: "",
+      property_location: "",
+      price: "",
+      property_desc: "",
+    },
+    {
+      property_img: property3,
+      property_name: "",
+      property_location: "",
+      price: "",
+      property_desc: "",
+    },
+    {
+      property_img: property4,
+      property_name: "",
+      property_location: "",
+      price: "",
+      property_desc: "",
+    },
+  ]);
+
+  return (
+    <>
+      <div>
+        <div className={styles.row_one}>
+          <div className={styles.left_line}>
+            <img src={left_line} width="400px" height="15px" className={styles.right_line_img} />
+          </div>
+          <div className={styles.title}>EXPLORE HOMES</div>
+          <div className={styles.right_line}>
+            <img src={right_line} width="400px" height="15px" className={styles.left_line_img} />
+          </div>
+        </div>
+        <div className={styles.row_two}>
+          {properties?.map((id, index) => (
+            <div className={styles.card}>
+              <div className={styles.property_img}>
+                <img src={properties[index].property_img} className={styles.banner_img}  width="400px" height="500px"/>
+              </div>
+              <div className={styles.property_name}>Luxurious Living Spaces</div>
+              <div className={styles.property_location}>101 Serene Avenue, Maplewood Gardens.</div>
+              <div className={styles.price}>90000</div>
+              <div className={styles.property_desc}>4 Bedroom 2 Bathroom 360 Sqft</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AllProjects;
