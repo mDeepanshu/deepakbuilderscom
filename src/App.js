4750df4efdfa2bc01b6b055017dc2b85fdef97f4
mdeepanshu <deepanshumalviya8964@gmail.com>
2025-04-06 14:05:16 +0530

    back button in all projcts

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         </>
       ) : (
         <>
-          <AllProjects></AllProjects>
+          <AllProjects handlePageChange={handlePage}></AllProjects>
         </>
       )}
       <Footer></Footer>
