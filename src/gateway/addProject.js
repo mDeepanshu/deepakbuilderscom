7337171070325f130811c82ab7820f6767dae553
mdeepanshu <deepanshumalviya8964@gmail.com>
2025-04-30 00:09:13 +0530

    endpoint changed

--- src/gateway/addProject.js
+++ src/gateway/addProject.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getData = async () => {
   try {
-    const response = await axios.get(`https://n6fpm9zzk8.execute-api.ap-south-1.amazonaws.com/dev/get-projects`);
+    const response = await axios.get(`https://iaorjab8qa.execute-api.ap-south-1.amazonaws.com/default/rkconstructions-dev-getAllProjects`);
     return response.data;  // return the data
   } catch (error) {
     console.error('Error fetching data:', error);
